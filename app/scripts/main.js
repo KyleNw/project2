87c01a2c614b89355d3f5d919d620fc1697ea019
KyleNw <kylejwilson@knights.ucf.edu>
2017-04-30 21:49:09 -0400

    Final

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,23 @@
 console.log('\'Allo \'Allo!');
 $(document).ready(function() {
 	$('#fullpage').fullpage();
+
+	
 });
+
+
+
+
+	$(function () {
+		$('.tlt').textillate({
+			loop: true,
+			minDisplayTime: 500,
+			initialDelay: 150,
+			// set the delay factor applied to each consecutive character
+			delayScale: .5,
+
+			// set the delay between each character
+			delay: 25
+		});
+	});
+	
